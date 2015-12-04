f0b4e2e7396e8c080f07adebe7f050079e96c586
goyyapazham <nsharadjaya@stuy.edu>
2015-12-04 14:30:03 -0500

    Initial Commit

---
+++ ListInt.java
@@ -0,0 +1,10 @@
+public interface ListInt {
+
+    void add(int newVal);
+    void add(int index, int newVal);
+    void remove(int index);
+    int size();
+    int get(int index);
+    int set(int index, int newVal);
+
+}
